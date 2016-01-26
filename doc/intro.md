68e9f99172e528a01cc1f9839e1cd38382e5cb39
lfn3 <lfalconer@liveops.com>
2016-01-26 19:07:18 +1300

    Inital commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cwim
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cwim/core_test.clj
@@ -0,0 +1,7 @@
+(ns cwim.core-test
+  (:require [clojure.test :refer :all]
+            [cwim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
