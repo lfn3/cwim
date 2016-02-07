7326df8bff2b90448d21f4d0eaf4bd0463d6dca4
lfn3 <lfalconer@liveops.com>
2016-02-08 11:08:46 +1300

    Server shutdown, testing some bits of the impl

--- src/cwim/core.clj
+++ src/cwim/core.clj
@@ -1,7 +1,8 @@
 (ns cwim.core
   (:require [org.httpkit.server :as hs]
             [org.httpkit.client :as hc]
-            [clojure.core.async :as a]))
+            [clojure.core.async :as a]
+            [clojure.edn :as edn]))
 
 ;TODO: implement me!
 (defn validate-req
@@ -25,12 +26,25 @@
       ((partial remove #(= %1 elem)))
       (concat [elem])))
 
-(defn send-impl [target msgs {:keys [epoch nodes]}]
-  (let [epoch @epoch]
-    (hc/post target
-             msgs
-             (fn [res]
-               (swap! nodes move-elem-to-end-of-coll target)))))
+(defn send-impl [target msg completed-fn]
+  (hc/post target
+           (pr-str msg)
+           (fn [res]
+             (if (= 200 (:status res))
+               (completed-fn (edn/read-string (:body res)))
+               (completed-fn false)))))
+
+(defn indirect-ping [node])
+
+(defn suspect [node]
+  ;TODO: add to suspect list
+  (indirect-ping node))
+
+(defn create-completion-fn [target internal-state]
+  (fn [body]
+    (if body
+      (swap! internal-state update :nodes move-elem-to-end-of-coll target)
+      (suspect target))))
 
 (defn ping
   ([{:keys [state internal-state cfg] :as srv}]
@@ -38,13 +52,12 @@
          target-nodes (take (:gossip-send-count cfg) (:nodes internal-state))
          msgs-to-send (-> (if-let [max (:max-msgs-per-epoch cfg)]
                             (take max (:send-queue internal-state))
-                            (:send-queue internal-state)))]
-     (swap! (:interal-state srv) #(vec (drop (count msgs-to-send) %1))) ;Remove messages we're about to send from server state
-     (dorun (map #(send-impl %1 msgs-to-send srv) target-nodes)))))
-
-(defn indirect-ping [node])
-
-(defn suspect [node])
+                            (:send-queue internal-state)))
+         send-fn (:send-fn cfg)]
+     (prn "Pinging to " target-nodes " with " msgs-to-send " using " send-fn)
+     ;TODO: replace below with something more robust...
+     (swap! (:internal-state srv) #(vec (drop (count msgs-to-send) %1))) ;Remove messages we're about to send from server state
+     (dorun (map #(send-fn %1 msgs-to-send (create-completion-fn %1 (:internal-state srv))) target-nodes)))))
 
 (defn dead [node])
 
@@ -53,43 +66,66 @@
   atom)
 
 (def default-port 64321)
-(def srv-map {:self       {:host nil
-                           :port nil}
-              :cfg        {:max-msgs-per-epoch nil
-                           :gossip-send-count  5
-                           :ping-time-in-ms    5000
-                           :send-fn            (fn [host msgs srv-map] nil)} ;TODO implement me!
-              :internal-state (atom {:nodes '() ; ::nodes {:host "0.0.0.0" :port 1234}
-                                     :send-queue []
-                                     :epoch 0})
-              :state      (atom {})
-              :shutdown   (fn [] (future nil))})            ;TODO implement me!
+
+(def default-cfg {:host               "0.0.0.0"
+                  :port               default-port
+                  :max-msgs-per-epoch nil                   ;nil = unlimted
+                  :gossip-send-count  5
+                  :ping-timer         5000
+                  :send-fn            send-impl             ;(fn [host msgs completion-fn]) -> nil
+                  :shutdown-fn        (fn [srv] nil)        ;TODO move this around?
+                  })
+
+(defn srv-map [] {:cfg            default-cfg
+                  :internal-state (atom {:nodes '() ; ::nodes {:host "0.0.0.0" :port 1234}
+                                         :send-queue []
+                                         :epoch 0})
+                  :state      (atom {})
+                  :shutdown   (fn [] (future nil))})            ;TODO implement me!
 
 ;; Public interface
+
 
 (defn start
   "Starts a server to listen for gossip messages, and for holding cfg.
   Hang onto the return from this, you'll need it when discovering nodes or sending.
-  Recommend you shove it in an atom."
-  ([] (start default-port))
-  ([port]
-   (let [srv-map (merge srv-map {:self {:host "0.0.0.0"
-                                        :port port}})
-         srv (hs/run-server (make-ping-handler srv-map) {:port port})
-         ping-time (get-in srv-map [:cfg :ping-time-in-ms])]
+  Recommend you shove it in an atom (or mount/component)."
+  ([] (start {}))
+  ([cfg]
+   (let [srv-map (assoc (srv-map) :cfg (merge default-cfg cfg))
+         srv (hs/run-server (make-ping-handler srv-map) {:port (get-in srv-map [:cfg :port])})
+         ping-time (get-in srv-map [:cfg :ping-timer])
+         control-ch (a/chan)]
      (a/go-loop []
-       (try
-         (ping srv)
-         (catch Exception e))
-       (a/<! (a/timeout ping-time))
-       (recur)))))
+       (let [[v ch] (a/alts! [(a/timeout ping-time)
+                              control-ch])]
+         (if (= ch control-ch)
+           (srv)
+           (do
+             (try
+               (ping srv-map)
+               (catch Exception e
+                 ;TODO Error handling strategy
+                 (prn e)))
+             (recur)))))
+     ;TODO shoving this in the cfg map doesn't seem right, precludes using a different server?
+     ;TODO put this somwhere else, the shutdown-fn should be produced by the server-start-fn,
+     ;TODO and invoked inside the go-loop above.
+     (assoc-in srv-map [:cfg :shutdown-fn] (fn [_] (a/put! control-ch :shutdown))))))
+
+(defn stop
+  "Stops a running server"
+  [srv]
+  ((get-in srv [:cfg :shutdown-fn] (fn [_]
+                                     (prn "Shutdown not defined, doing nothing...")
+                                     nil)) srv))
 
 (defn add-node
   "Add another node to gossip to"
   ([srv host] (add-node srv host default-port))
   ([srv host port] (-> srv
-                     (update :state swap-return-atom! update ::nodes conj {:host host :port port})
-                     (update :state swap-return-atom! update ::nodes shuffle))))
+                     (update :internal-state swap-return-atom! update :nodes conj {:host host :port port})
+                     (update :internal-state swap-return-atom! update :nodes shuffle))))
 
 (defn send-msg
   ([srv key val]
--- test/cwim/core_test.clj
+++ test/cwim/core_test.clj
@@ -1,10 +1,43 @@
 (ns cwim.core-test
   (:require [clojure.test :refer :all]
-            [cwim.core :refer :all]))
+            [cwim.core :refer :all]
+            [clojure.core.async :as a]))
 
 (deftest add-node-test
   (testing "Add a single node"
-    (let [srv-map {:state (atom {:cwim.core/nodes '()})}
-          updated-map (add-node srv-map "0.0.0.0")]
-      (is (contains? (set (:cwim.core/nodes @(:state updated-map)))
-                     {:host "0.0.0.0" :port default-port})))))
+    (let [updated-map (add-node (srv-map) "0.0.0.0")]
+      (is (contains? (set (:nodes @(:internal-state updated-map)))
+                     {:host "0.0.0.0" :port default-port}))))
+
+  (testing "Adding nodes reshuffles order"
+    (let [matching-sort-count (atom 0)
+          srv-map (srv-map)]
+      (doseq [host (range 60)]
+        (add-node srv-map (str "0.0.0." host))
+        (when (= (:nodes @(:internal-state srv-map))
+                  (sort-by :host (:nodes @(:internal-state srv-map))))
+          (swap! matching-sort-count inc)))
+      (is (> 4 @matching-sort-count)))))
+
+(deftest start-server-test
+  (testing "Send-fn is called on ping"
+    (let [call-count (atom 0)
+          cfg (merge default-cfg {:send-fn    (fn [_ _ _] (swap! call-count inc))
+                                  :ping-timer 500})
+          srv (start cfg)]
+      (add-node srv "0.0.0.0")
+      (a/<!! (a/timeout (* 3 (:ping-timer cfg))))
+      (stop srv)
+      (is (< 0 @call-count))))
+
+  (testing "Ping stops after shutdown"
+    (let [call-count (atom 0)]
+      (with-redefs [ping (fn [srv] (swap! call-count inc))]
+        (let [cfg (assoc default-cfg :ping-timer 500)
+              srv (start cfg)]
+          (a/<!! (a/timeout (* 3 (:ping-timer cfg))))
+          (let [current-count @call-count]
+            (stop srv)
+            (a/<!! (a/timeout (* 3 (:ping-timer cfg))))
+            (is (= current-count @call-count))
+            (is (< 0 @call-count))))))))
